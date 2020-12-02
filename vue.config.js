0e07682abf2e7d34627512578f622909fbebf5e1
Pedro <ytzeleoner@gmail.com>
2020-12-02 09:12:24 +0100

    new folder

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-    publicPath: './accionadashboard/dist'
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/accionadashboard/'
+    : '/',
+    outputDir: 'docs'
 };
